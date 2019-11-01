4a52e34cc86716d8375cdcff1ef1a1ac40bbde4f
thiagoalessio <talessio@redhat.com>
2019-11-01 11:45:18 +0100

    Bump to version 0.1.0 Looks like 0.0.9 was already uploaded at some point

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ setuptools.setup(
     name='rh-ocp-build-data-validator',
     author='AOS ART Team',
     author_email='aos-team-art@redhat.com',
-    version='0.0.9',
+    version='0.1.0',
     description='Validation of ocp-build-data Image & RPM declarations',
     long_description_content_type='text/x-rst',
     long_description=open('README.rst').read(),
-    url='https://gitlab.cee.redhat.com/openshift-art/tools/ocp-build-data-validator',  # noqa: E501
+    url='https://github.com/openshift/ocp-build-data-validator',
     license='Apache License, Version 2.0',
     packages=['validator', 'validator.schema'],
     entry_points={'console_scripts': [
