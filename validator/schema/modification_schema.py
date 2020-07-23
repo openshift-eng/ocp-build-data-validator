3779e1df100b441f623b5395d4943e2097c42086
thiagoalessio <talessio@redhat.com>
2020-07-23 13:29:24 +0200

    Allow empty value for `replacement` modification

--- validator/schema/modification_schema.py
+++ validator/schema/modification_schema.py
@@ -17,7 +17,7 @@ def modification(file):
                       Or(*valid_modification_commands),
                   ],
                   Optional('match'): And(str, len),
-                  Optional('replacement'): And(str, len),
+                  Optional('replacement'): Or(None, str),
                   Optional('source'): And(str, len),
                   Optional('path'): And(str, len),
                   Optional('overwriting'): bool,
