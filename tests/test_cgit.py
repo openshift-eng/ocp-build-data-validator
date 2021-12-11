b608319c8a7bd8d5696b17bc39c107af2096bc53
Joep van Delft <jdelft@redhat.com>
2021-12-11 13:08:44 +0100

    Call cgit tests cgit

--- tests/test_cgit.py
+++ tests/test_cgit.py
@@ -1,10 +1,10 @@
 import unittest
 from flexmock import flexmock
 
-from validator import distgit
+from validator import cgit
 
 
-class TestDistgit(unittest.TestCase):
+class TestCgit(unittest.TestCase):
 
     def setUp(self):
         self.group_cfg = {
@@ -13,56 +13,56 @@ class TestDistgit(unittest.TestCase):
             'vars': {'MAJOR': 4, 'MINOR': 2},
         }
 
-        (flexmock(distgit.support)
+        (flexmock(cgit.support)
             .should_receive('resource_is_reacheable')
             .and_return(True))
 
-        (flexmock(distgit.support)
+        (flexmock(cgit.support)
             .should_receive('resource_exists')
             .and_return(True))
 
     def test_image_artifact(self):
-        (url, err) = distgit.validate('images/foo.yml', {}, self.group_cfg)
+        (url, err) = cgit.validate('images/foo.yml', {}, self.group_cfg)
         self.assertEqual(url, 'http://my.cgit.endpoint/containers/foo')
         self.assertIsNone(err)
 
     def test_rpm_artifact(self):
-        (url, err) = distgit.validate('rpms/bar.yml', {}, self.group_cfg)
+        (url, err) = cgit.validate('rpms/bar.yml', {}, self.group_cfg)
         self.assertEqual(url, 'http://my.cgit.endpoint/rpms/bar')
         self.assertIsNone(err)
 
     def test_unknown_artifact(self):
-        (url, err) = distgit.validate('unknown/foo.yml', {}, self.group_cfg)
+        (url, err) = cgit.validate('unknown/foo.yml', {}, self.group_cfg)
         self.assertEqual(url, 'http://my.cgit.endpoint/???/foo')
         self.assertIsNone(err)
 
     def test_file_with_custom_namespace(self):
         data = {'distgit': {'namespace': 'apbs'}}
-        (url, err) = distgit.validate('images/foo.yml', data, self.group_cfg)
+        (url, err) = cgit.validate('images/foo.yml', data, self.group_cfg)
         self.assertEqual(url, 'http://my.cgit.endpoint/apbs/foo')
         self.assertIsNone(err)
 
     def test_file_with_additional_extensions(self):
-        (url, err) = distgit.validate('images/x.apb.y.yml', {}, self.group_cfg)
+        (url, err) = cgit.validate('images/x.apb.y.yml', {}, self.group_cfg)
         self.assertEqual(url, 'http://my.cgit.endpoint/containers/x')
         self.assertIsNone(err)
 
     def test_cgit_endpoint_not_reacheable(self):
-        (flexmock(distgit.support)
+        (flexmock(cgit.support)
             .should_receive('resource_is_reacheable')
             .and_return(False))
 
-        (url, err) = distgit.validate('images/my-img.yml', {}, self.group_cfg)
+        (url, err) = cgit.validate('images/my-img.yml', {}, self.group_cfg)
         self.assertEqual(url, 'http://my.cgit.endpoint/containers/my-img')
         self.assertEqual(err, ('This validation must run from a network '
                                'with access to http://my.cgit.endpoint'))
 
     def test_cgit_repository_doesnt_exist(self):
-        (flexmock(distgit.support)
+        (flexmock(cgit.support)
             .should_receive('resource_exists')
             .and_return(False))
 
-        (url, err) = distgit.validate('images/my-img.yml', {}, self.group_cfg)
+        (url, err) = cgit.validate('images/my-img.yml', {}, self.group_cfg)
         self.assertEqual(url, 'http://my.cgit.endpoint/containers/my-img')
         self.assertEqual(err, ('Corresponding DistGit repo was not found.\n'
                                "If you didn't request a DistGit repo yet, "
@@ -74,38 +74,38 @@ class TestDistgit(unittest.TestCase):
     def test_use_branch_declared_on_file(self):
         expected_url = 'http://my.cgit.endpoint/containers/img'
 
-        (flexmock(distgit)
+        (flexmock(cgit)
             .should_receive('branch_exists')
             .with_args('my-custom-branch', expected_url)
             .and_return(True))
 
         data = {'distgit': {'branch': 'my-custom-branch'}}
-        (url, err) = distgit.validate('images/img.yml', data, self.group_cfg)
+        (url, err) = cgit.validate('images/img.yml', data, self.group_cfg)
         self.assertEqual(url, expected_url)
         self.assertIsNone(err)
 
     def test_use_branch_declared_on_file_with_vars(self):
         expected_url = 'http://my.cgit.endpoint/containers/img'
 
-        (flexmock(distgit)
+        (flexmock(cgit)
             .should_receive('branch_exists')
             .with_args('my-4-branch.2', expected_url)
             .and_return(True))
 
         data = {'distgit': {'branch': 'my-{MAJOR}-branch.{MINOR}'}}
-        (url, err) = distgit.validate('images/img.yml', data, self.group_cfg)
+        (url, err) = cgit.validate('images/img.yml', data, self.group_cfg)
         self.assertEqual(url, expected_url)
         self.assertIsNone(err)
 
     def test_use_branch_from_group_cfg(self):
         expected_url = 'http://my.cgit.endpoint/containers/my-img'
 
-        (flexmock(distgit)
+        (flexmock(cgit)
             .should_receive('branch_exists')
             .with_args('rhaos-4.2-rhel-999', expected_url)  # defined on setUp
             .and_return(True))
 
-        (url, err) = distgit.validate('images/my-img.yml', {}, self.group_cfg)
+        (url, err) = cgit.validate('images/my-img.yml', {}, self.group_cfg)
         self.assertEqual(url, expected_url)
         self.assertIsNone(err)
 
@@ -113,11 +113,11 @@ class TestDistgit(unittest.TestCase):
         branch_url = ('http://my.cgit.endpoint/containers/my-img/'
                       'log?h=rhaos-4.2-rhel-999')  # defined on setUp
 
-        (flexmock(distgit.support)
+        (flexmock(cgit.support)
             .should_receive('resource_exists')
             .with_args(branch_url)
             .and_return(False))
 
-        (url, err) = distgit.validate('images/my-img.yml', {}, self.group_cfg)
+        (url, err) = cgit.validate('images/my-img.yml', {}, self.group_cfg)
         self.assertEqual(url, 'http://my.cgit.endpoint/containers/my-img')
         self.assertEqual(err, 'Branch rhaos-4.2-rhel-999 not found on DistGit')
