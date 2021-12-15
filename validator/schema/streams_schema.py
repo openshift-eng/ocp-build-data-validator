f355213904eab303f641a48f4e5f11d47124d854
Luke Meyer <lmeyer@redhat.com>
2021-12-15 12:29:14 -0500

    make upstream_image_base optional

--- validator/schema/streams_schema.py
+++ validator/schema/streams_schema.py
@@ -3,7 +3,7 @@ from schema import Schema, Optional, And, SchemaError
 STREAMS_SCHEMA = {
         And(str, len): {
             'image': And(str, len),
-            'upstream_image_base': And(str, len),
+            Optional('upstream_image_base'): And(str, len),  # won't need if we do not transform
             'upstream_image': And(str, len),
             Optional('mirror'): bool,
             Optional('transform'): And(str, len),
